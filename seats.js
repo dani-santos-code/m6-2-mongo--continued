79f8db4d0552637c0cd75d67e2f0a9773934dc5e
Dani Santos <santos.adaniele@gmail.com>
2020-04-21 17:24:22 -0400

    add seats logic to generate data for the DB

---
+++ seats.js
@@ -0,0 +1,13 @@
+const seats = [];
+const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+for (let r = 0; r < row.length; r++) {
+  for (let s = 1; s < 13; s++) {
+    seats.push({
+      _id: `${row[r]}-${s}`,
+      price: 225,
+      isBooked: false,
+    });
+  }
+}
+
+module.exports = { seats };
